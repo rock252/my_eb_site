f7d414b9d84684b744ec10668d519e1791f360b5
shivam <rockbox252@gmail.com>
2018-05-12 00:37:39 +0530

    Added

--- my_eb_site/settings.py
+++ my_eb_site/settings.py
@@ -77,7 +77,7 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
        'default': {
              'ENGINE':'django.db.backends.postgresql_psycopg2',
-             'NAME': os.environ['aak5eraxpiynsf'],
+             'NAME': os.environ['RDS_DB_NAME'],
              'USER': os.environ['udnkhatola'],
              'PASSWORD': os.environ['hostad123'],
              'HOST': os.environ['ebdb'],
