4b3f5e0b936c9bd35dece97acea955c923e2cf98
shivam <rockbox252@gmail.com>
2018-05-11 23:32:45 +0530

    Added EB database config and requirements files

--- my_eb_site/settings.py
+++ my_eb_site/settings.py
@@ -81,6 +81,35 @@ DATABASES = {
 }
 
 
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+       'default': {
+             'ENGINE':'django.db.backends.postgresql_psycopg2',
+             'NAME': os.environ['hostaddb'],
+             'USER': os.environ['udnkhatola'],
+             'PASSWORD': os.environ['hostad123'],
+             'HOST': os.environ['hostad'],
+             'PORT': os.environ['5432'],
+             }
+          }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    } 
+
+
+
+
+
+
+
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -116,5 +145,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATIC_URL = '/static/'
+
