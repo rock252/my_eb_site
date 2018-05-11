56bf2309f3979bfe65790988f1677580f01d365a
shivam <rockbox252@gmail.com>
2018-05-12 00:11:08 +0530

    Added

--- my_eb_site/settings.py
+++ my_eb_site/settings.py
@@ -73,15 +73,6 @@ WSGI_APPLICATION = 'my_eb_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
        'default': {
